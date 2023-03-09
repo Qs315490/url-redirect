55a4a7024746b7771196e358104bfa9ad5d9e02f
Qs315490 <qs315490@qq.com>
2023-03-09 22:10:58 +0800

    登录接口返回用户验证信息

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,8 @@ const route = new Route();
 
 const database = require('../lib/database');
 const log = require('../lib/log');
+const jwt = require('jsonwebtoken')
+const config = require('../lib/config')
 
 route.prefix('/api')
 
@@ -17,12 +19,22 @@ route.post('/register', async (ctx, next) => {
 route.post('/login', async (ctx, next) => {
     const body = ctx.request.body
     const status = await database.userLogin(body.name, body.passwd)
+    if (status.code === 0) {
+        status.data = jwt.sign(status.data, config.get('key', (value) => {
+            if (value === undefined || value === "") {
+                log('服务器 key 未设置, 使用默认 key: "test". 请及时修改.')
+                return 'test'
+            }
+        }), {
+            expiresIn: 24 * 60 * 60 // 24*60*60s = 1d
+        })
+    }
     ctx.body = status
     log(status)
     next()
 })
 
-route.get('/url',async (ctx,next)=>{
+route.get('/url', async (ctx, next) => {
     const token = ctx.request.query.token
     const status = await database.getUrl(token)
     ctx.body = status
